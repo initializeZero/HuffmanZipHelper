f103be605a3508543d34fbdc44d484ec930821c8
JackYanx <1085473804@qq.com>
2018-05-03 14:41:05 +0800

    CreateProject

---
+++ HuffmanZipHelper/HuffmanZipHelper/commen.h
@@ -0,0 +1,25 @@
+/*
+* Author: initializeZero CopyRight 2016~2020
+* 本项目遵循GPL 2.0 共享协议
+* 可以使用于非商业用途
+* 请勿用于商业用途,保留一切追究法律责任的权利
+* md5(Email)=c1e156dd78b2126ea4b616c74aec9294
+*/
+
+
+#ifndef COMMEN_H
+#define COMMEN_H
+
+#include <cstdio>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <memory>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+#endif // COMMEN_H
